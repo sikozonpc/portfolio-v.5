e399d5e6e479cd093b21909c057f19d38859134d
sikozonpc <sikozonpc@gmail.com>
2019-02-22 11:42:10 +0000

    button to top added and certs page

--- js/navbar.js
+++ js/navbar.js
@@ -8,3 +8,20 @@ function responsiveNavbar() {
         x.className = "nav-items topnav";
     }
 };
+
+// When the user scrolls down x from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    document.getElementById("topButton").style.display = "block";
+  } else {
+    document.getElementById("topButton").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function toTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
